e7dd20cc6598d91a0bfd82c0fff3ed0029cf2f67
Joseph <jLegand@retrorabbit.co.za>
2019-03-11 11:50:07 +0200

    Added Create Organisation file as master root
    Added snippets for each component

---
+++ src/Components/SuperAdmin/CustomerAddress.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';   
+
+class CustomerAddress extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default CustomerAddress;
---
+++ src/Components/SuperAdmin/PhysicalAddress.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class PhysicalAddress extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default PhysicalAddress;
