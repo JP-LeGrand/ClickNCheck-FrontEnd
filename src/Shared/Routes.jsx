e3652a4a273f5e6d5dc1f0e50db83d951930c1f7
Tafdav <tvurayayi@retrorabbit.co.za>
2019-03-11 23:42:41 +0200

    made forgot password page

--- src/Shared/Routes.jsx
+++ src/Shared/Routes.jsx
@@ -1,16 +1,15 @@
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import Home from '../Components/Home/Home';
-import Login from '../Components/Shared/Login/Login';
+//import Home from '../Components/Home/Home';
 import Consent from '../Components/Candidate/Consent/Consent';
 import Consented from '../Components/Candidate/Consented/Consented';
+import ForgotPassword from '..//Components/Shared/ForgotPassword/ForgotPassword';
 import React from 'react';
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/login" exact component={Login} />
+                <Route path="/" exact component={ForgotPassword}/>
                 <Route path="/candidate/consent" exact component={Consent} />
                 <Route path="/candidate/consented" exact component={Consented} />
                 <Route render={() => <Redirect to="/"/>}/>
