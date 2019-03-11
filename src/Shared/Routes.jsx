6566ad0f3d94aa11a5ff0858c02da07ddf0181bc
Kudzai Muranga <murangakudzai@gmail.com>
2019-03-11 16:17:57 +0200

    Madee next button work

--- src/Shared/Routes.jsx
+++ src/Shared/Routes.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Home from '../Components/Home/Home';
+import MainContainer from '../Recruiter/RecruiterVerificationRequest/MainContainer';
 import React from 'react';
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Home}/>
+                <Route path="/" exact component={MainContainer}/>
                 <Route render={() => <Redirect to="/"/>}/>
             </Switch>
         </BrowserRouter>
