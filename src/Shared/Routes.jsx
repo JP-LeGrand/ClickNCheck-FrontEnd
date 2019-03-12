7a8e52fc42f825fad1f36db3bb70e9d0eba55e82
Kudzai Muranga <murangakudzai@gmail.com>
2019-03-12 08:24:22 +0200

    Added changes

--- src/Shared/Routes.jsx
+++ src/Shared/Routes.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Home from '../Components/Home/Home';
-import MainContainer from '../Recruiter/RecruiterVerificationRequest/MainContainer';
+import CaptureCandidateDetails from '../Recruiter/RecruiterVerificationRequest/CaptureCandidateDetails';
 import React from 'react';
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={MainContainer}/>
+                <Route path="/" exact component={CaptureCandidateDetails}/>
                 <Route render={() => <Redirect to="/"/>}/>
             </Switch>
         </BrowserRouter>
