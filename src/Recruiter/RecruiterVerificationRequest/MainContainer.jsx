77acf00ed6736faf4e66bfe3036eccb7c27f351b
Xolani Dlamini <xdlamini@retrorabbit.co.za>
2019-03-14 04:35:25 +0200

    "added validation functionality"

--- src/Recruiter/RecruiterVerificationRequest/MainContainer.jsx
+++ src/Recruiter/RecruiterVerificationRequest/MainContainer.jsx
@@ -1,7 +1,7 @@
-/* eslint-disable indent */
 import React from 'react';
 import './MainContainerStyle.scss';
 import XLSX from 'xlsx';
+import { RecruiterConstants } from './recruiterConstants';
 
 class MainContainer extends React.PureComponent {
 
@@ -13,19 +13,20 @@ class MainContainer extends React.PureComponent {
             email : '',
             id : '',
             number: '',
-            tableErrors: { email:'', id:'' },
+            tableErrors: { email:'', id:'', phone: '' },
             emailValid: false,
             idValid: false,
             numberValid: false,
-            tableValid: false
-
+            tableValid: false,
+            fieldID: 'FieldValue',
+            fieldEmail: 'FieldValue',
+            fieldPhone: 'FieldValue',
         };
-       this.submit = this.submit.bind(this);
-       this.bulk = this.bulk.bind(this);
-       this.handleUserInput = this.handleUserInput.bind(this);
-       this.handleFormValidation = this.handleFormValidation.bind(this);
+        this.submit = this.submit.bind(this);
+        this.bulk = this.bulk.bind(this);
+        this.handleUserInput = this.handleUserInput.bind(this);
+  
     }
-
 
     handleUserInput (event) {
         const name = event.target.name;
@@ -34,50 +35,75 @@ class MainContainer extends React.PureComponent {
             () => { 
                 this.validateField(name, value);
             });
-      }
-     handleFormValidation(){
-        console.log(this.state.tableValid);
+    }
+    validateField(fieldName, value){
+        let emailValid = this.state.emailValid;
+        let idValid = this.state.idValid;
+        let numberValidator = this.state.numberValid;
+        let tableValidationErrors = this.state.tableErrors;
+           
+        switch ( fieldName ){
+        case 'email' : 
+            emailValid = value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+            tableValidationErrors.email = emailValid ? true : false;
+            break;   
 
-      }
-      validateField(fieldName, value){
-            let emailValid = this.state.emailValid;
-            let idValid = this.state.idValid;
-            let numberValidator = this.state.numberValid;
-            let tableValidationErrors = this.state.tableErrors;
+        case 'id' : 
+            idValid = value.length === RecruiterConstants.idNumberLen;
+            tableValidationErrors.id = idValid ? true : false;
+            break;   
 
-            switch ( fieldName ){
-                case 'email' : 
-                emailValid = value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
-                tableValidationErrors.email = emailValid ? true : false;
-                console.log('validationg email');
-                break;   
-
-                case 'id' : 
-                idValid = value.length === 13;
-                tableValidationErrors.id = idValid ? true : false;
-                break;   
-
-                case 'phone' :
-                numberValidator = 'not entered';
-                break; 
+        case 'phone' :
+            numberValidator = value.length === RecruiterConstants.phoneNumberLen;
+            tableValidationErrors.phone = numberValidator ? true : false;
+            break; 
                    
-                default: 
-                break;
-            }
-
-            this.setState({
-                 tableErrors: tableValidationErrors,
-                emailValid: emailValid,
-                 idValid: idValid  
-            }, this.validateTable);
-
-      }
-
-      validateTable() {
+        default: 
+            break;
+        }
         this.setState({
-            tableValid: this.state.emailValid && this.state.idValid
+            tableErrors: tableValidationErrors,
+            emailValid: emailValid,
+            numberValid: numberValidator,
+            idValid: idValid  
+        }, this.validateTable);
+    }
+
+    validateTable() {
+        this.setState({
+            tableValid: this.state.emailValid && this.state.idValid && this.state.numberValid
         });
-      }
+        if (this.state.idValid === false ) {
+            this.setState({
+                fieldID: 'InvalidField'   
+            });
+        } else {
+            this.setState({
+                fieldID: 'FieldValue'   
+            });
+        }
+
+        if (this.state.emailValid === false){
+            this.setState({
+                fieldEmail: 'InvalidField',
+            });
+        } else {
+            this.setState({
+                fieldEmail: 'FieldValue'   
+            });
+        }
+
+        if (this.state.numberValid === false) {
+            this.setState({
+                fieldPhone: 'InvalidField'  
+            }); 
+        } else {
+            this.setState({
+                fieldPhone: 'FieldValue'   
+            });
+        }
+        
+    }
 
     submit(){
         let inputFile = document.getElementById('getFile');  
@@ -94,108 +120,110 @@ class MainContainer extends React.PureComponent {
             this.setState({
                 excelRows: exceRows,
                 getFile: true
-              });
-            
+            });
+
+            this.state.excelRows.map((name) => {
+                return (
+                    this.validateField('email', name.Email),
+                    this.validateField('id', name.IDorPassport), 
+                    this.validateField('phone', name.Phone)
+                    
+                );
+            });           
         };
-     reader.readAsBinaryString(inputFile.files[0]);
+        reader.readAsBinaryString(inputFile.files[0]);
         
     }
 
-    bulk(){
-        return(
-                        <fieldset className="field1 current">
-                        <h3>Capture Candidate Details</h3>
-                        <div id="uploadSwitch"> 
-                            <button className="btn btn-primary active" id="individual">INDIVIDUALLY</button>
-                            <button className="btn btn-default" id="bulk">BULK</button>
-                        </div>
-    
-                        <div id="bulkForm">
-                            <div className="upload-area" id="uploadfile">
-                                <img src={require('../../Assets/upload-file.svg')} alt="upload files here" />
-                                <h3>Drag and Drop files to upload</h3><br/>
-                                <p>Upload only excel documents </p>
-                                <div className="upload-btn-wrapper">
-                                    <button className="btn btn-primary" id="selectFiles">Select Files</button>
-                                    <input type="file" name="file" id="getFile" onChange={() => this.submit() } />
-                                    {/* <button id="check" onClick = {this.submit}> </button> */}
-                                   
-                               </div>
-                                <a><img src={require('../../Assets/downloadFile.svg')} alt="download-fav" />Download Excel Template</a>
-                                </div>
-                                </div>
-                             </fieldset>
-        );
-
-    } 
-   
     review(){
-
         
-        return(
+        return (
             <fieldset className="field1 current">
-            <table className = "table">
-                              <thead>
-                                  <tr>
-                                     <th>First Full Name</th>
-                                      <th>Surname</th>
-                                      <th>Maiden Name</th>
-                                      <th scope="col">ID/Passport</th>
-                                      <th scope="col">Email</th>
-                                      <th scope="col">Phone</th>
-                                  </tr>
-                              </thead>
-                              <tbody id ="testing">
-                              {this.state.excelRows.map((name) => {
-                                  return (
-                                      // eslint-disable-next-line react/jsx-key
-                                      <tr>
-                                      <td><input type="text" name="name" defaultValue = {name.FirstName} onChange={(event) => this.handleUserInput(event)}/></td>
-                                      <td><input type="text" name="surname" defaultValue = {name.Surname} onChange={(event) => this.handleUserInput(event)}/></td>
-                                      <td><input type="text" name="madein" defaultValue = {name.MaidenSurname} onChange={(event) => this.handleUserInput(event)}/></td>
-                                      <td><input type="text" name="id" defaultValue = {name.IDorPassport} onChange={(event) => this.handleUserInput(event)}/></td>
-                                      <td><input type="text" name="dob" defaultValue = {name.Birthday} onChange={(event) => this.handleUserInput(event)}/></td>
-                                      <td><input type="email" name="email" defaultValue = {name.Email} onChange={(event) => this.handleUserInput(event)}/></td>
-                                      <td><input type="text" name="phone" defaultValue = {name.Phone} onChange={(event) => this.handleUserInput(event)}/></td>
-                                  </tr>
-                                  );
-                              })}
+                <table className = "ImportTable">
+                    <thead className="Headers">
+                        <tr className="Headers">
+                            <th>First Full Name</th>
+                            <th>Surname</th>
+                            <th>Maiden Name</th>
+                            <th>ID/Passport</th>
+                            <th>Birthday</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                        </tr>
+                    </thead>
+                    <tbody className="Headers">
+                        {this.state.excelRows.map((name, key) => {
+                            return (
+                                    
+                                <tr className="Shape" key={key.IDs} >  
+                                    <td className="fieldContainer" ><input className="FieldValue" type="text" name="name" defaultValue = {name.FirstName} onChange={(event) => this.handleUserInput(event)}/></td>
+                                    <td className="fieldContainer"><input className="FieldValue" type="text" name="surname" defaultValue = {name.Surname} onChange={(event) => this.handleUserInput(event)}/></td>
+                                    <td className="fieldContainer"><input className="FieldValue" type="text" name="madein" defaultValue = {name.MaidenSurname} onChange={(event) => this.handleUserInput(event)}/></td>
+                                    <td className="fieldContainer"><input className={this.state.fieldID} type="text" name="id" defaultValue = {name.IDorPassport} onChange={(event) => this.handleUserInput(event)}/></td>
+                                    <td className="fieldContainer"><input className="FieldValue" type="text" name="dob" defaultValue = {name.Birthday} onChange={(event) => this.handleUserInput(event)}/></td>
+                                    <td className="fieldContainer"><input className={this.state.fieldEmail} type="text" name="email" defaultValue = {name.Email} onChange={(event) => this.handleUserInput(event)}/></td>
+                                    <td className="fieldContainer"><input className={this.state.fieldPhone} type="text" name="phone" defaultValue = {name.Phone} onChange={(event) => this.handleUserInput(event)}/></td>
+                                </tr>
+                            );
+                        })}
                                   
-                              </tbody>
-                              <validationErrors tableErrors={this.state.tableErrors} />
-                              <button type="submit" className="btn btn-primary" onClick={this.handleFormValidation}>Sign up</button>
-                          </table> 
+                    </tbody>
+                    <validationErrors tableErrors={this.state.tableErrors} />
+                    <button type="submit" className="btn btn-primary" disabled={!this.state.tableValid}>Sign up</button>
+                </table> 
           
-                            </fieldset>  
+            </fieldset>  
                            
         );
     
     }
-    render() {
 
-        const fileState = this.state.getFile;
-        console.log(fileState);
-        let button;
-        
-       return (
+    bulk(){
+        return (
+            <fieldset className="field1 current">
+                <h3>Capture Candidate Details</h3>
+                <div id="uploadSwitch"> 
+                    <button className="btn btn-primary active" id="individual">INDIVIDUALLY</button>
+                    <button className="btn btn-default" id="bulk">BULK</button>
+                </div>
+    
+                <div id="bulkForm">
+                    <div className="upload-area" id="uploadfile">
+                        <img src={require('../../Assets/upload-file.svg')} alt="upload files here" />
+                        <h3>Drag and Drop files to upload</h3><br/>
+                        <p>Upload only excel documents </p>
+                        <div className="upload-btn-wrapper">
+                            <button className="btn btn-primary" id="selectFiles">Select Files</button>
+                            <input type="file" name="file" id="getFile" onChange={() => this.submit() } />
+                            
+                        </div>
+                        <a href="https://cncdocuments.blob.core.windows.net/recruiters/CandidateTemplate.xlsx" download><img src={require('../../Assets/downloadFile.svg')} alt="download-fav" />Download Excel Template</a>
+                    </div>
+                </div>
+            </fieldset>
+        );
+
+    } 
+
+    render() {
+      
+        return (
             <div className="bodyPage">
                 <div id="formContainer">
                     <ul id="progress_bar">
                         <li className="active">Create Job Profile Name</li>
                         <li>Candidate Details</li>
-                        <li></li> 
-                    </ul>
-                    {!this.state.getFile ? this.bulk() : this.review()}
-                 
+                        <li>Next Steps</li> 
+                      
+                    </ul>           
+                    {!this.state.getFile ? this.bulk() : this.review() }  
+                               
                 </div>
-         <div>
-           
-           </div>
-
-             <div id="buttonFooter">
+         
+                <div id="buttonFooter">
                     <button id = "prev">BACK</button>
                     <button id = "next" onClick={()=>this.next()}>NEXT</button>
-                </div>
+                </div> 
             </div>
         );
     }
