4622c391054a98db8e345fd02ddf060e0f863af0
Joseph <jLegand@retrorabbit.co.za>
2019-03-19 16:11:47 +0200

    Deleted Routes.Orig
    Changed redirection for assign recruiter cancel button
    
    Added a Congratulations.jsx
    Added a Congratulations.scss

--- src/Shared/Routes.jsx
+++ src/Shared/Routes.jsx
@@ -16,6 +16,7 @@ import AddRemoveChecks from '../Components/Recruiter/RecruiterVerificationReques
 import CreateJobProfile from '../Components/Admin/CreateJobProfile/CreateJobProfile';
 import AdminPage from '../Components/Admin/AdminPage/AdminPage';
 import assignRecruiters from '../Components/Admin/AssignRecruiters/assignRecruiters';
+import Congratulations from '../Components/Admin/Congratulations/Congratulations';
 const Routes = () => {
     return (
         <BrowserRouter>
@@ -37,6 +38,7 @@ const Routes = () => {
                 <Route path="/Admin/CreateJobProfile" exact component={CreateJobProfile} />
                 <Route path="/Admin/AdminPage" exact component={AdminPage} />
                 <Route path="/Admin/AssignRecruiters" exact component={assignRecruiters}/>
+                <Route path="/Admin/Congratulations" exact component={Congratulations}/>
                 <Route render={() => <Redirect to="/" />} />
             </Switch>
         </BrowserRouter>
