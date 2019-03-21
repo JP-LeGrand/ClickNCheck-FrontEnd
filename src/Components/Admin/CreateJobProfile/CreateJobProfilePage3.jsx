e3418ee31c273076c0565cd6b352734c4af45bbf
Keoabetswe Morake <kmorake@retrorabbit>
2019-03-21 15:18:14 +0200

    Styling done

--- src/Components/Admin/CreateJobProfile/CreateJobProfilePage3.jsx
+++ src/Components/Admin/CreateJobProfile/CreateJobProfilePage3.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import NavBar from '../AdminNavBar/adminNavBar';
 import './CreateJobProfile.scss';
+import gridview from '../../../Assets/gridview.svg';
+import saveImg from '../../../Assets/save.svg';
+import dragImg from '../../../Assets/drag.svg';
+import { sortableContainer, sortableElement } from 'react-sortable-hoc';
+import arrayMove from 'array-move';
+
+const SortableItem = sortableElement(({ value }) => <li><img src={gridview}/>{value}</li>);
+
+const SortableContainer = sortableContainer(({ children }) => {
+    return <ul>{children}</ul>;
+});
 
 class CreateJobProfilePage3 extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
             jobProfileName: localStorage.getItem('jobProfileName'),
-            JobProfile: {}
+            JobProfile: {},
+            checks: [ 'TransUnion Credit Report','Experian Credit Report','XDS Credit Report','Criminal Check - Attach Fingerprints Captured' ]
         };
 
         this.nextStep = this.nextStep.bind(this);
@@ -27,12 +39,19 @@ class CreateJobProfilePage3 extends React.PureComponent {
     nextStep() {
         //make the job profile object
         //and store them in the local storage
-        window.location = '/Admin/AssignRecruiters';
+        window.location = '/Admin/CreateJobProfilePage4';
     }
 
+    onSortEnd = ({ oldIndex, newIndex }) => {
+        this.setState(({ checks }) => ({
+            checks: arrayMove(checks, oldIndex, newIndex),
+        }));
+    };
+
     render() {
+        const { checks } = this.state;
         return (
-            <div className="bodyPage">
+            <div className="createJobProfile">
                 <NavBar />
                 <div id="spanHolder">
                     <span className="New-Verification-Req">New Job Profile</span>
@@ -48,9 +67,21 @@ class CreateJobProfilePage3 extends React.PureComponent {
                     <h4 className="Re-order-Check-Seque text-style-1">{this.state.jobProfileName}</h4>
                     <p className="The-checks-with-the">The checks with the highest candidate failure / dropout rates will be done first</p>
                     <hr className="Line" />
+                    <div className="flex-container">
+                        <img src={dragImg} alt="gridview image"></img>
+                        <p className="Change-the-sequence">Change the sequence by dragging items to re-order. </p>
+                    </div>
+                    <div id="Sortable-Checks">
+                        <SortableContainer onSortEnd={this.onSortEnd}>
+                            {checks.map((value, index) => <SortableItem key={`item-${index}`} index={index} value={value} />)}
+                        </SortableContainer>
+                    </div>
                     <div id="buttonFooter">
                         <button id="prev" onClick={this.prevStep}>BACK</button>
-                        <button id="save" onClick={this.saveProgress}>Save and continue later</button>
+                        <div id="saveButtonDiv">
+                            <img src={saveImg} alt="save img"/>
+                            <button id="save" onClick={this.saveProgress}>Save and continue later</button>
+                        </div>
                         <button id="next" onClick={this.nextStep}>NEXT</button>
                     </div>
                 </div>
