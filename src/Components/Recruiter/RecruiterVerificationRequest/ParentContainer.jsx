4a0c4af75c5726f93b4f3f08e761a1826fdd4959
Xolani Dlamini <xdlamini@retrorabbit.co.za>
2019-03-28 09:12:14 +0200

    ''

--- src/Components/Recruiter/RecruiterVerificationRequest/ParentContainer.jsx
+++ src/Components/Recruiter/RecruiterVerificationRequest/ParentContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import NavBar from '../NavBar/NavBar';
 import './ParentContainerStyle.scss';
 import Footer from '../../Shared/Footer/Footer';
@@ -9,6 +9,8 @@ import * as CandidateActions from './CandidateActions';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
+import { ToastContainer, toast } from 'mdbreact';
+
 class ParentContainer extends React.PureComponent {
 
     constructor(props) {
@@ -29,10 +31,10 @@ class ParentContainer extends React.PureComponent {
         this.setState({
             loading: true
         });
-        let candidates = {
-            candidates: this.props.candidateArray
-        };
-        this.props.sendBulk("1", candidates);
+        let candidates = this.props.candidateArray
+      
+        let ver_check = localStorage.getItem('ver_check');
+        this.props.sendBulk('1', candidates);
 
     }
     backstep() {
@@ -41,60 +43,67 @@ class ParentContainer extends React.PureComponent {
 
     render() {
         return (
-            <div className="candidateNav">
-                <NavBar />
-                <div className="bodyPage">
-                    <h2 id="newVTitile"><b>New Verification Request</b></h2>
-                    <h3 id="JPTitle">Job Profile</h3>
-                    <div className="formBox">
-                        <fieldset className="field1 current">
-                            <div id="singleForm">
-                                <div className="">
-                                    <ul id="progress_bar">
-                                        <li className="active">Select Verification Checks</li>
-                                        <li className="active"><b>Candidate Details</b></li>
-                                        <li>Next Steps</li>
-                                    </ul>
-                                    <label className="candidateDetails">Capture Candidate Details</label><br />
-                                    {this.props.candidateState ?
-                                        <div className="switchButton">
-                                            <button className="indi" id="individual" onClick={() => this.changeDiv(true)}>INDIVIDUAL</button>
-                                            <button className="bulk" id="bulk" onClick={() => this.changeDiv(false)}> BULK</button>
-
-                                        </div>
-                                        :
-                                        this.props.fileState ?
-
+            <Fragment>
+                <ToastContainer 
+                    hideProgressBar={true}
+                    newestOnTop={true}
+                    autoClose={5000}
+                />
+                <div className="candidateNav">
+                    <NavBar />
+                    <div className="bodyPage">
+                        <h2 id="newVTitile"><b>New Verification Request</b></h2>
+                        <h3 id="JPTitle">Job Profile</h3>
+                        <div className="formBox">
+                            <fieldset className="field1 current">
+                                <div id="singleForm">
+                                    <div className="">
+                                        <ul id="progress_bar">
+                                            <li className="active">Select Verification Checks</li>
+                                            <li className="active"><b>Candidate Details</b></li>
+                                            <li>Next Steps</li>
+                                        </ul>
+                                        <label className="candidateDetails">Capture Candidate Details</label><br />
+                                        {this.props.candidateState ?
                                             <div className="switchButton">
-                                                <button className="indiNew" id="individual" onClick={() => this.changeDiv(true)}>INDIVIDUAL</button>
-                                                <button className="bulkNew" id="bulk" onClick={() => this.changeDiv(false)}> BULK</button>
+                                                <button className="indi" id="individual" onClick={() => this.changeDiv(true)}>INDIVIDUAL</button>
+                                                <button className="bulk" id="bulk" onClick={() => this.changeDiv(false)}> BULK</button>
 
                                             </div>
                                             :
-                                            <div className="">
-                                                <label className="reviewDetails">Review Candidate Details</label><br /><br />
-                                                <label className="reviewDetails"><strong>{this.props.fileSize} Entries</strong> (click to edit)</label><br />
-                                            </div>
+                                            this.props.fileState ?
 
-                                    }
-                                    <section>
-                                        {this.props.candidateState ? <CaptureCandidateDetails /> : <MainContainer />}
-                                    </section>
+                                                <div className="switchButton">
+                                                    <button className="indiNew" id="individual" onClick={() => this.changeDiv(true)}>INDIVIDUAL</button>
+                                                    <button className="bulkNew" id="bulk" onClick={() => this.changeDiv(false)}> BULK</button>
+
+                                                </div>
+                                                :
+                                                <div className="">
+                                                    <label className="reviewDetails">Review Candidate Details</label><br /><br />
+                                                    <label className="reviewDetails"><strong>{this.props.fileSize} Entries</strong> (click to edit)</label><br />
+                                                </div>
+
+                                        }
+                                        <section>
+                                            {this.props.candidateState ? <CaptureCandidateDetails /> : <MainContainer />}
+                                        </section>
+                                    </div>
                                 </div>
-                            </div>
 
-                        </fieldset>
-                        <div id="buttonFooter">
-                            <button id="prev" onClick={this.backstep}>BACK</button>
-                            <button id="next" onClick={this.sendCandidate} >SUBMIT</button>
-                            <div className="loading">
-                                {this.state.loading && <img src={rollingImg} id="spinner" alt="loading..." />}
+                            </fieldset>
+                            <div id="buttonFooter">
+                                <button id="prev" onClick={this.backstep}>BACK</button>
+                                <button id="next" onClick={this.sendCandidate} >SUBMIT</button>
+                                <div className="loading">
+                                   {this.state.loading && <img src={rollingImg} id="spinner" alt="loading..." />}
+                                </div>`
                             </div>
                         </div>
+                        <Footer />
                     </div>
-                    <Footer />
                 </div>
-            </div>
+            </Fragment>                      
         );
     }
 }
--- src/Shared/State/InitialState.js
+++ src/Shared/State/InitialState.js
@@ -44,6 +44,8 @@ const InitialState = {
         candidateBody: [],
         fileState: true,
         fileSize : '',
+        idValid : '',
+        emailValid : '',
         tableValid : false
     },
 
