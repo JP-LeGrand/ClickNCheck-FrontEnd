a5a006bec86cce5fb8060d14fc8e734ed118c01f
Joseph <jLegand@retrorabbit.co.za>
2019-03-28 11:26:27 +0200

    Added React Application Insights package.json
    Added tracking to the App component App.jsx
    
    Added Insights AI to Index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,24 @@ import * as serviceWorker from './Shared/serviceWorker';
 import { createAppState } from './Shared/State/AppState';
 import { Provider } from 'react-redux';
 import App from './Components/App/App';
+import ReactAI from 'react-appinsights';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
-ReactDOM.render(<Provider store={createAppState()}><App /></Provider>, document.getElementById('root'));
+const history = createBrowserHistory();
+ReactAI.init({instrumentationKey:'fa2f4b1a-34ae-4964-9ec9-461b50c51e04', history });
+ReactAI.setAppContext({ urlReferrer: document.referrer });
+var appInsights = ReactAI.ai()
+appInsights.trackPageView();
+appInsights.trackEvent();
+appInsights.trackException();
+appInsights.trackMetric();
+appInsights.trackDependency();
+ReactDOM.render(<Provider store={createAppState()}>
+<Router history={history}>
+    <App />
+</Router>
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
