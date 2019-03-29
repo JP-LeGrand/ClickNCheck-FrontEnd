e329d62e364acf0b46def5e0989e5aa10372d919
Tafdav <tvurayayi@retrorabbit.co.za>
2019-03-29 09:19:00 +0200

    footer now visible in all admin pages

--- src/Components/Admin/Users/Users.jsx
+++ src/Components/Admin/Users/Users.jsx
@@ -5,9 +5,10 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import UserResults from './UserResults';
-import { BASE_URL, GET_ASSIGNED_JOB_PROFILES, GET_ALL_USERS, GET_RECRUITERS, GET_MANAGERS, GET_OPERATORS } from '../../../Shared/Constants';
+import { BASE_URL, GET_ALL_USERS, GET_RECRUITERS, GET_MANAGERS, GET_OPERATORS } from '../../../Shared/Constants';
 import './Users.scss';
 import imgLoading from '../../../Assets/Rolling.svg';
+import Footer from '../../Shared/Footer/Footer';
 
 class Users extends Component {
     constructor(props) {
@@ -206,6 +207,7 @@ class Users extends Component {
                         </div>
                     </div>
                 </div>
+                <Footer />
             </div>
             
         );
