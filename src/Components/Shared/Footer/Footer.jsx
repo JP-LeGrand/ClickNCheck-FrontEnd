cf0f8ad2533eba0580c549253ddc94558afd7330
Joseph <jLegand@retrorabbit.co.za>
2019-04-03 14:25:58 +0200

    Made the footer responsive using bootstrap
    Added customised styling for the footer

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -5,11 +5,27 @@ import './Footer.scss';
 class Footer extends React.PureComponent {
     render() {
         return ( 
-            <footer id="footer">   
-                <a id="left" href="#">T's & C's</a>
-                <a id="center" href="#">ClicknCheck 2019</a>
-                <a id="right" href="#">Contact Us</a>
-            </footer>
+            <div>
+                <footer className="footer">   
+                    <div className="container">  
+                        
+                        <div className="d-flex">
+                            <div className="ml-auto mr-auto ">
+                                <a className="clickncheck" href="#">ClicknCheck 2019</a>
+                            </div>
+                            <div className=" mr-5">
+                                <a className="clickncheck" href="#">T's & C's</a>
+                            </div>
+                            <div className="">
+                                <a className="clickncheck" href="#">Contact Us</a>
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                </footer>
+            </div>
+            
         );
     }
 }
